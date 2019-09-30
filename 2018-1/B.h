c584e2860b340b85a6706def386fc833ca5569aa
alulab14 <alulab14@inf.pucp.edu.pe>
2019-09-30 12:40:17 -0500

    L5 2018-1

---
+++ 2018-1/B.h
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   B.h
+ * Author: alulab14
+ *
+ * Created on 30 de septiembre de 2019, 12:36 PM
+ */
+
+#ifndef B_H
+#define B_H
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+void leer(char ***p);
+void impr(char ***p);
+
+#endif /* B_H */
+
---
+++ 2018-1/build/Debug/MinGW-Windows/B.o.d
@@ -0,0 +1,1 @@
+build/Debug/MinGW-Windows/B.o: B.cpp
